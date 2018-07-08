aabd4edea89f2407038a0056d19569ad1ab086b9
Beth <bethsalvatore2018@gmail.com>
2018-07-08 18:16:36 -0400

    first commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+//app.vue looks for a template, script, style
+
+<template>
+  <div id="app1">
+    <img src="./assets/logo.png">
+    <HelloWorld msg="Beth loves bootcamp!"/>
+
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld
+
+  }
+}
+
+</script>
+
+<style>
+#app1 {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <input class='input-form' v-model='newTask' v-on:keyup.enter="addItem" placeholder='Enter task here'>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    newTask: '',
+    tasks: ['one', 'two', 'three'],
+  },
+  data: function () {
+    return { mutableList: JSON.parse(this.tasks)};
+  },
+  methods:{
+    addItem: function(){
+      this.tasks.push(this.newTask);
+      this.newTask = '';
+      console.log(tasks);
+    }
+  }
+
+}
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
